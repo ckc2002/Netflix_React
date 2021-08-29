1ccc85d3687852aa94f39879c26d903f199a1f39
Chaitanya Kumar Chandra <84777895+ckc2002@users.noreply.github.com>
2021-08-29 14:45:51 +0530

    Add files via upload

---
+++ src/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Card(web) {
+  return (
+  <>
+    <div className="cards">
+      <div className="card">
+        <img src= {web.imgsrc} alt="myPic" className="card_img" />
+        <div className="card_info">
+          <span className="card_category">{web.title}</span>
+          <h3 className="card_title"> {web.sname}</h3>
+          <a href = {web.link} target="_blank">
+            <button> Watch Now</button>
+          </a>
+        </div>
+      </div>
+    </div>
+ </>)
+}
+
+export default Card;
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Card";
+import "./index.css";
+import Sdata from "./Sdata";
+
+
+
+ReactDOM.render(
+  <>
+
+    <h1 className= "heading_style">Top 15 Netflix Series</h1>   
+    {Sdata.map((val) => {
+      return (
+        <Card imgsrc = {val.imgsrc}
+        title = {val.title}
+        sname = {val.sname}
+        link = {val.links}
+        />
+      )
+    })
+    }
+  </>
+ ,
+ document.getElementById('root')
+);
